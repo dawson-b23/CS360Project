3e4c3b229f102249e795cc3257cb44b54f8170ec
dawson-b23 <98564761+dawson-b23@users.noreply.github.com>
2022-05-01 15:04:37 -0700

    idek

--- proj/websiteCS360/events/admin.py
+++ proj/websiteCS360/events/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Customer
 from .models import Vendor
 #from .models import Wishlist
-from .models import Requirement
+#from .models import Requirement
 from .models import Product
 #from .models import Service
 from .models import Delivery
@@ -10,7 +10,7 @@ from .models import Delivery
 admin.site.register(Customer)
 admin.site.register(Vendor)
 #admin.site.register(Wishlist)
-admin.site.register(Requirement)
+#admin.site.register(Requirement)
 admin.site.register(Product)
 #admin.site.register(Service)
 admin.site.register(Delivery)
